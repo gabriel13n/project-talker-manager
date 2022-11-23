709af07c540921d5bfcb763a19ef8cfc45d43d6e
Gabriel Cantuária <gabriel13n@gmail.com>
2022-11-23 16:52:42 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readTalkerData } = require('./utils/fsUtils')
+
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -16,3 +20,8 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 //
+
+app.get('/talker', async (req, res) => {
+  const allTalkers = await readTalkerData()
+  return res.status(HTTP_OK_STATUS).json(allTalkers);
+})
